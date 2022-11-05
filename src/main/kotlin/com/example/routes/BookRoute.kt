f0bd6968b3442fcf8195eb5edd81afddd116bb57
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-05 18:28:06 +0900

    ch10-3 REST APIの実装 - JSONでPOSTリクエストを送信する

--- src/main/kotlin/com/example/routes/BookRoute.kt
+++ src/main/kotlin/com/example/routes/BookRoute.kt
@@ -2,12 +2,20 @@ package com.example.routes
 
 import io.ktor.server.application.*
 import io.ktor.server.locations.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BookResponse(
+    val id: Long,
+    val title: String,
+    val author: String
+)
+
+@Serializable
+data class RegisterRequest(
     val id: Long,
     val title: String,
     val author: String
@@ -20,6 +28,14 @@ fun Route.bookRoute() {
         get<BookLocation> { request ->
             val response = BookResponse(request.bookId, "Kotlin入門", "Kotlin太郎")
             call.respond(response)
+        }
+
+        post("/register") {
+            val request = call.receive<RegisterRequest>()
+
+            // 登録処理
+
+            call.respondText("registerd. id=${request.id} title=${request.title} author=${request.author}")
         }
     }
 }
