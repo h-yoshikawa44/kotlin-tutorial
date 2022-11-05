e3f3b9d4a4766934d5b1fa23e53e4548c66a253e
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-05 17:44:15 +0900

    ch10-2 Ktorの導入 - Locationを使用して型安全なパラメータ取得 - パスの共通化

--- src/main/kotlin/com/example/routes/UserRoute.kt
+++ src/main/kotlin/com/example/routes/UserRoute.kt
@@ -5,12 +5,23 @@ import io.ktor.server.locations.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-@Location("/user/{id}")
-data class GetUserLocation(val id: Long)
+@Location("/user")
+class UserLocation {
+    @Location("/{id}")
+    data class GetLocation(val id: Long)
+
+    @Location("/detail/{id}")
+    data class GetDetailLocation(val id: Long)
+}
 
 fun Route.userRoute() {
-    get<GetUserLocation> { param ->
+    get<UserLocation.GetLocation> { param ->
         val id = param.id
         call.respondText("id=$id")
+    }
+
+    get<UserLocation.GetDetailLocation> { param ->
+        val id = param.id
+        call.respondText("getDetail id=$id")
     }
 }
