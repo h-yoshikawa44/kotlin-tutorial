d9aec7593bdd7e602d227b49fc0a20a907a2ba43
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-05 18:44:51 +0900

    ch10-4 認証機構の実装 - 認証のプラグイン追加

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,6 +5,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.auth.*
 import io.ktor.server.locations.*
 import io.ktor.server.plugins.contentnegotiation.*
 
@@ -14,6 +15,18 @@ fun main() {
 }
 
 fun Application.module() {
+    install(Authentication) {
+        basic {
+            validate { credentials ->
+                if (credentials.name == "user" && credentials.password == "password") {
+                    // 認証成功時にセッションに保持される認証情報
+                    UserIdPrincipal(credentials.name)
+                } else {
+                    null
+                }
+            }
+        }
+    }
     install(Locations)
     install(ContentNegotiation) {
         json()
