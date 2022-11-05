fad11b85ec328aa83b1e0981d812a5c7953d483a
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-05 17:33:00 +0900

    ch10-2 Ktorの導入 - Locationを使用して型安全なパラメータ取得 - プラグインを追加

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import io.ktor.server.locations.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -11,5 +12,6 @@ fun main() {
 }
 
 fun Application.module() {
+    install(Locations)
     configureRouting()
 }
