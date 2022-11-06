30d50133c9e7b9d74e6de3a375125c7c9fe483a1
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-06 17:06:41 +0900

    ch12-4 データ駆動テストを使う - forAllを使用して複数のパラメータをテストする

--- src/test/kotlin/com/example/NumberTestByStringSpec.kt
+++ src/test/kotlin/com/example/NumberTestByStringSpec.kt
@@ -1,6 +1,10 @@
 package com.example
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
 import io.kotest.matchers.shouldBe
 
 // StringSpec を継承すると String Spec 実装が可能となる
@@ -14,6 +18,31 @@ class NumberTestByStringSpec : StringSpec() {
         "isOdd:: when value is even number then return false" {
             val number = Number(2)
             number.isOdd() shouldBe false
+        }
+        // データ駆動テスト
+        "isRange:: when value in range then return true" {
+            forAll(
+                table(
+                    headers("value"),
+                    row(1),
+                    row(10)
+                )
+            ) { value ->
+                val number = Number(value)
+                number.isRange(1, 10) shouldBe true
+            }
+        }
+        "isRange:: when value not in range then return false" {
+            forAll(
+                table(
+                    headers("value"),
+                    row(0),
+                    row(11)
+                )
+            ) { value ->
+                val number = Number(value)
+                number.isRange(1, 10) shouldBe false
+            }
         }
     }
 }
