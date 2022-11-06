a4f5b4ee32610ab24d202d406326879cf65acef5
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-06 17:17:48 +0900

    ch12-5 MockKを使用してモック化する - テストコードの実装

---
+++ src/test/kotlin/com/example/UserServiceTest.kt
@@ -0,0 +1,29 @@
+package com.example
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+
+class UserServiceTest : StringSpec() {
+    init {
+        "createMessage:: when user name is exist then return message" {
+            // モックオブジェクトの生成
+            val userRepository = mockk<UserRepository>()
+            val target = UserService(userRepository)
+
+            val id = 100
+
+            // モックオブジェクトの関数の挙動を定義
+            every { userRepository.findName(any()) } returns "Kotest"
+
+            // 戻り値の検証
+            val result = target.createMessage(id)
+
+            result shouldBe "Hello Kotest"
+            // モックオブジェクトの関数呼び出しの検証
+            verify { userRepository.findName(id) }
+        }
+    }
+}
